1db82e4db5981fcb8df0aac6bc730f9e6bbdab88
OPELsinus <92687099+OPELsinus@users.noreply.github.com>
2025-03-15 23:30:19 +0500

    .

--- src/components/Board.js
+++ src/components/Board.js
@@ -132,7 +132,7 @@ const Board = () => {
           ))}
         </div>
         {/* Lobby Name */}
-        <div className="lobby-name">Lobby Name</div>
+        <div className="lobby-name">Lobby Name Lol</div>
       </div>
 
       {/* Main Grid: Numbers + Special Buttons */}
